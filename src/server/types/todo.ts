fdd68fb821b754eb50921365cd027db7703959ee
Yuki Sakaguchi <sakaguchi.ba55.0ne@gmail.com>
2023-06-18 23:33:31 +0900

    Update Todoコンポーネント実装

--- src/server/types/todo.ts
+++ src/server/types/todo.ts
@@ -1,4 +1,10 @@
 import { z } from "zod";
+import type { inferRouterOutputs } from "@trpc/server";
+import type { AppRouter } from "@/server/api/root";
+
+type RouterOutput = inferRouterOutputs<AppRouter>; // AppRouter の出力の型を推論するためのヘルパータイプ
+type allTodosOutput = RouterOutput["todo"]["all"]; // Todo を全件取得するルーターの出力の型
+export type Todo = allTodosOutput[number]; // Todoの型
 
 export const createInput = z.string().min(1).max(50);
 
